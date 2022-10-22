14eab47ceaad0e1f7ee765e8c936c0dcb20abc6d
Nimah <busayonimota1603@gmail.com>
2022-10-22 11:10:15 -0700

    [Test]: check for errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,15 +12,3 @@ statuss = {
 def status():
     """Return the status of the server"""
     return statuss
-
-@app_views.route('/stats', strict_slashes=False)
-def statistics():
-    """Return the statistics of the objects"""
-    return statist = {
-    "amenities": storage.count('Amenity'),
-    "cities": storage.count('City'),
-    "places": storage.count('Place'),
-    "reviews": storage.count('Review'),
-    "states": storage.count('State'),
-    "users": storage.count('User')
-    }
