0885f8af560891eca3a3f723174157a5b1b26477
Nimah <busayonimota1603@gmail.com>
2022-10-22 11:04:02 -0700

    [Test]: check for errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,14 +8,14 @@ statuss = {
     "status": "OK"
 }
 
-statist = {
+"""statist = {
     "amenities": storage.count('Amenity'),
     "cities": storage.count('City'),
     "places": storage.count('Place'),
     "reviews": storage.count('Review'),
     "states": storage.count('State'),
     "users": storage.count('User')
-}
+}"""
 
 @app_views.route('/status', strict_slashes=False)
 def status():
