cd56ff2e3dc72e807700c37dd59c8a8d3556a4da
Nimah <busayonimota1603@gmail.com>
2022-10-22 11:34:08 -0700

    [Feats]: Handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Import Blueprint"""
 
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,10 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response({"error": "Not found"}, 404)
 
 
 if __name__ == '__main__':
