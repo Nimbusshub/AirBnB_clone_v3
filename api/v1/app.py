7d7904e989c3d2ec2bc15eab7b90e41c5f45b10b
Nimah <busayonimota1603@gmail.com>
2022-10-22 08:01:07 -0700

    [Feats]: add new contents

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""The root of my domain"""
 
-from flask import Flasks
-from models import
+from flask import Flask
+from flask import Blueprint, render_template
+from models import storage
+from api.v1.views import app_views
+from flask import teardown_appcontext
+
 app = Flask(__name__)
 
+@app.teardown_appcontext
+@app.teardown_
