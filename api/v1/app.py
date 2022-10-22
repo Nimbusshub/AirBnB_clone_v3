520f2fd2d498874921aebc08382df17d1d6b4892
Nimah <busayonimota1603@gmail.com>
2022-10-22 16:33:35 +0100

    [Feats]: Add new features

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,26 @@ from api.v1.views import app_views
 from flask import teardown_appcontext
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
-@app.teardown_
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    host, port = '', 0
+    if HBNB_API_HOST:
+        host = HBNB_API_HOST
+    else:
+        host = '0.0.0.0'
+    if HBNB_API_PORT:
+        port = HBNB_API_PORT
+    else:
+        port = 5000
+
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask
+
+status = {
+    "status": "OK"
+}
+
+
+@app_views.route('/status'):
+def return_status():
+    return status
