1ab16b317423a56e08771ff74617110529a9f4cb
Nimah <busayonimota1603@gmail.com>
2022-10-22 11:41:48 -0700

    [fix]: checking for errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Import Blueprint"""
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,7 +17,8 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response({"error": "Not found"}, 404)
+    """Handle 404 error and render a JSON output"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
