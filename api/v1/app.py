792ce25186b243bc60a0463bec741d094f5568cd
Nimah <busayonimota1603@gmail.com>
2022-10-22 17:37:18 +0100

    [Fix]: Remove unnecessary imported modules

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""The root of my domain"""
+"""Import Blueprint"""
 
 from flask import Flask
-from flask import Blueprint, render_template
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import teardown_appcontext
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,14 +19,9 @@ def teardown_db(exception):
 if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
-    host, port = '', 0
+    host, port = '0.0.0.0', 5000
     if HBNB_API_HOST:
         host = HBNB_API_HOST
-    else:
-        host = '0.0.0.0'
     if HBNB_API_PORT:
         port = HBNB_API_PORT
-    else:
-        port = 5000
-
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,12 @@
 
 from api.v1.views import app_views
 
-status = {
+statuss = {
     "status": "OK"
 }
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Return the status of the server"""
-    return status
+    return statuss
